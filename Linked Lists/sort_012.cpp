9ac20eb258ed200d43385ad831a264ec0448326a
Rajdeep1999GitHub <rajdeep1999.srivastav@gmail.com>
2022-10-11 19:21:46 +0530

    11oct

--- Linked Lists/sort_012.cpp
+++ Linked Lists/sort_012.cpp
@@ -30,3 +30,45 @@ Node* sortList(Node *head)
 */
 //this above solution will change the data
 //which is not required, hence we will think of another approach
+class Solution
+{   
+    private:
+    void insertAtTail(Node* &tail, Node* curr){
+        tail->next=curr;
+        tail=curr;
+    }
+    
+    void delNode(Node* &temp){
+        temp->next=NULL;
+        delete temp;
+    }
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node *head) {
+        Node* zeroHead = new Node(-1);
+        Node* zeroTail = zeroHead;
+        Node* oneHead = new Node(-1);
+        Node* oneTail = oneHead;
+        Node* twoHead = new Node(-1);
+        Node* twoTail = twoHead;
+        
+        Node* curr = head;
+        while(curr != NULL){
+            if(curr->data==0) insertAtTail(zeroTail ,curr);
+            if(curr->data==1) insertAtTail(oneTail ,curr);
+            if(curr->data==2) insertAtTail(twoTail ,curr);
+            
+            curr=curr->next;
+        }
+        if(oneHead->next!=NULL) zeroTail->next=oneHead->next, oneTail->next=twoHead->next;
+        else zeroTail->next=twoHead->next;
+        twoTail->next=NULL;
+        
+        head=zeroHead->next;
+        
+        delete(zeroHead);
+        delete(oneHead);
+        delete(twoHead);
+        return head;
+    }
+};
