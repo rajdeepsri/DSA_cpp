08b12f50aca64fb691b8016f8c2c797d13c9d6f6
Rajdeep1999GitHub <rajdeep1999.srivastav@gmail.com>
2022-10-30 23:36:00 +0530

    special stack construction

---
+++ Stack/O(1)_stack_construction.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+class SpecialStack {
+    // Define the data members.
+    stack<int> s;
+    int mini;
+    public:  
+    void push(int data) {
+        if(s.empty()){
+            s.push(data);
+            mini=data;
+        }
+        else{
+            if(data<mini){
+                s.push(2*data - mini);
+                mini=data;
+            }
+            else{
+                s.push(data);
+            }
+        }
+    }
+
+    int pop() {
+        if(s.empty()) return -1;
+        
+        int curr = s.top();
+        s.pop();
+        if(curr>mini){
+            return curr;
+        }
+        else{
+            int prevMin = mini;
+            int val = 2*mini-curr;
+            mini = val;
+            return prevMin;
+        }
+    }
+
+    int top() {
+        if(s.empty()) return -1;
+        int curr=s.top();
+        if(curr<mini){
+            return mini;
+        }
+        else{
+            return curr;
+        }
+    }
+
+    bool isEmpty() {
+        return s.empty();
+    }
+
+    int getMin() {
+        if(s.empty()) return -1;
+        return mini;
+    }  
+};
