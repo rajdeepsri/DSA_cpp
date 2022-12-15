bed2325f80d9d5fd286646297895131a46b9523d
Rajdeep1999GitHub <rajdeep1999.srivastav@gmail.com>
2022-12-15 22:33:51 +0530

    Balanced_string_gfg

--- strings/Balanced_string_gfg.cpp
+++ strings/Balanced_string_gfg.cpp
@@ -1,34 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+const string alpha = "abcdefghijklmnopqrstuvwxyz";
 bool isSumOdd(int n){
-        if(n>9){
-            int num=n;
-            int res=0;
-            while(num != 0){
-                res += num%10;
-                num /= 10;
-            }
-            if(res%2==0) return false;
+    if(n>9){
+        int num=n;
+        int res=0;
+        while(num != 0){
+            res += num%10;
+            num /= 10;
         }
-        else{
-            if(n%2==0) return false;
-        }
-        return true;
+        if(res%2==0) return false;
     }
+    else{
+        if(n%2==0) return false;
+    }
+    return true;
+}
 
 string BalancedString(int N) {
     int num=N;
-    string alpha = "abcdefghijklmnopqrstuvwxyz";
     string ans="";
         
    //if number greater than 26, repeat instruction till size == 26
    while(num>26){
-       int index=0;
-       while(ans.size() <= 26) ans.push_back(alpha[index++]);
-       num -= 26;
-   }
-   
+        int index=0;
+        int count=26;
+        while(count--) ans.push_back(alpha[index++]);
+        num -= 26;
+    }
+//    cout<<"num is "<<num<<endl;
+//    cout<<"ans size "<<ans.size()<<endl;
+//    cout<<"string is "<<ans<<endl;
+
    //number is even
    if(num%2==0){
        //put the aplhabets N/2 from start and end
@@ -46,7 +49,7 @@ string BalancedString(int N) {
    //number is odd
    else{
        //digits sum is odd
-       if(isSumOdd(num)){
+       if(isSumOdd(N)){
            //(n-1)/2 from start
            int count = (num-1)/2;
            int index=0;
@@ -74,7 +77,7 @@ string BalancedString(int N) {
    }
 
 int main(){
-    int n=21;
-    cout<<"ans is "<<BalancedString(n);
+    int n=57;
+    cout<<BalancedString(n);
     return 0;
 }
